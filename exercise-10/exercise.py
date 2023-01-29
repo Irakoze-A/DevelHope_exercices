9d2fc1c99b0f555b58b93a4ab9faa2ef8d62e9bf
aristide <irakozestide@gmail.com>
2023-01-30 01:20:12 +0300

    exercises num 7,8,9,10

---
+++ exercise-10/exercise.py
@@ -0,0 +1,8 @@
+hello = 'Hello!'
+name = 'Jhon Doe'
+age = '40'
+
+
+print( len(hello))
+print( len(name))
+print( len(age))
---
+++ exercise-9/exercise.py
@@ -0,0 +1,7 @@
+a = 12
+b = 'Hello'
+
+temp = b
+b = a
+a = temp
+print(a, b)
