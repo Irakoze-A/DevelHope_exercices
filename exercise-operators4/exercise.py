da84c0ad8c99a02a5ef242ef5c5c9e9494cae350
aristide <irakozestide@gmail.com>
2023-01-30 13:39:03 +0300

    operators exercises

---
+++ exercise-operators4/exercise.py
@@ -0,0 +1,5 @@
+#print(not ("testing" == "testing" ... "Mario" == "Cool Guy")) # Should print True
+
+#Insert the appropriate logical operator in order to print True.
+
+print(not ("testing" == "testing" and "Mario" == "Cool Guy"))
