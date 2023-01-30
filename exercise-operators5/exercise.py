cb5e896f22009a1a39049b0841ea8343d3d56a70
aristide <irakozestide@gmail.com>
2023-01-30 13:53:42 +0300

    exercise operators 5

---
+++ exercise-operators5/exercise.py
@@ -0,0 +1,8 @@
+#Initialize and print the sentence variable using the appropriate operators.
+
+firstName = "Mario"
+lastName = "Rossi"
+
+sentence = ' '.join( [firstName  ,lastName ])
+
+print(sentence) # Should print "Mario Rossi"
