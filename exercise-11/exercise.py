28556196c308f7fd7714133899ee5eba33bd470c
aristide <irakozestide@gmail.com>
2023-02-03 00:22:24 +0300

    correction for exercice about Variables 11

--- exercise-11/exercise.py
+++ exercise-11/exercise.py
@@ -3,12 +3,13 @@ b = 'tom' #uppercase
 c = 'LAURA' #lowercase
 question = 'How are you' #change o in e
 age_question = 'How old are you?' #use the correct method to create a string for each word
+
 print(a, b, c, question, age_question)
 
 b = b.upper()
 a = a.capitalize()
 c = c.lower()
-
+age_question = age_question.split()
 question = question.replace('o','e')
 
 print(f'{a} {b} {c} {question} {age_question}')
